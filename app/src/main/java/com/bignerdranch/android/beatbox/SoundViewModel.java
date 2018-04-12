ac3cddd116b3f327c1ccdf93467a88e85bc0238d
andreykoleber <andrei.koleber@gmail.com>
2018-04-13 00:10:50 +0600

    Add new custom theme

--- app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
@@ -12,10 +12,6 @@ public class SoundViewModel extends BaseObservable{
         mBeatBox = beatBox;
     }
 
-    public Sound getSound() {
-        return mSound;
-    }
-
     public void setSound(Sound sound) {
         mSound = sound;
         notifyChange();
